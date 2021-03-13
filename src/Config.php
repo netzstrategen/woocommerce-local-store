fce173df0f0286761f7a1b03d31d8c75143c49c1
Mauricio Urrego <mauricio@netzstrategen.com>
2021-03-13 17:47:56 +0100

    Enables retrieving ids by name and type intersection.

--- src/Config.php
+++ src/Config.php
@@ -59,6 +59,26 @@ class Config {
     return $ids;
   }
 
+  public static function getStoreIdsByName(string $name): array {
+    $ids = [];
+    foreach (Config::get() as $id => $store_config) {
+      if ($store_config['name'] === $name) {
+        $ids[] = $id;
+      }
+    }
+    return $ids;
+  }
+
+  public static function getStoreIdsByNameAndType(string $name, string $type): array {
+    $ids = [];
+    foreach (Config::get() as $id => $store_config) {
+      if ($store_config['name'] === $name && $store_config['type'] === $type) {
+        $ids[] = $id;
+      }
+    }
+    return $ids;
+  }
+
   /**
    * Returns the content of a file, given its path.
    *
